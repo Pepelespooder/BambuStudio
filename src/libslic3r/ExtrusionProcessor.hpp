34f2cb576dd4f035daaf8387212e6fbbb65a5164
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2025-09-29 04:23:27 -0400

    Fix remaining commented include path in ExtrusionProcessor.hpp

--- src/libslic3r/ExtrusionProcessor.hpp
+++ src/libslic3r/ExtrusionProcessor.hpp
@@ -4,7 +4,7 @@
 // This algorithm is copied from PrusaSlicer, original author is Pavel Mikus(pavel.mikus.mail@seznam.cz)
 
 #include "AABBTreeLines.hpp"
-//#include "../SupportSpotsGenerator.hpp"
+//#include "SupportSpotsGenerator.hpp"
 #include "libslic3r.h"
 #include "ExtrusionEntity.hpp"
 #include "Layer.hpp"
