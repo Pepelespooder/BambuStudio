24ab0b196df38424d4d44c5a8c6ad7ccb9d44858
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2025-09-29 12:20:04 -0400

    Add loop_node_range to ExtrusionEntityCollection
    Introduces the loop_node_range member to ExtrusionEntityCollection and ensures it is properly copied and assigned in constructors and assignment operators. This change supports tracking additional state within the collection.

--- src/libslic3r/ExtrusionEntityCollection.cpp
+++ src/libslic3r/ExtrusionEntityCollection.cpp
@@ -33,6 +33,8 @@ ExtrusionEntityCollection& ExtrusionEntityCollection::operator=(const ExtrusionE
     for (size_t i = 0; i < this->entities.size(); ++i)
         this->entities[i] = this->entities[i]->clone();
     this->no_sort       = other.no_sort;
+    this->is_reverse    = other.is_reverse;
+    this->loop_node_range = other.loop_node_range;
     return *this;
 }
 
