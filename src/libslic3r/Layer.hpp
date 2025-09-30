7264a380eb2b13ebdfd0ecad185f56ec7bafda88
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2025-09-29 20:37:07 -0400

    Remove CurledLine struct and CurledLines alias
    Deleted the CurledLine struct and its associated CurledLines type alias from Layer.hpp, likely as part of code cleanup or refactoring to remove unused or obsolete code.

--- src/libslic3r/Layer.hpp
+++ src/libslic3r/Layer.hpp
@@ -27,14 +27,6 @@ class PrintObject;
 struct PerimeterRegion;
 using PerimeterRegions = std::vector<PerimeterRegion>;
 
-struct CurledLine
-{
-    Point a, b;
-    float curled_height;
-};
-
-using CurledLines = std::vector<CurledLine>;
-
 namespace FillAdaptive {
     struct Octree;
 };
