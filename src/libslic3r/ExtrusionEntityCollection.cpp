41376ce9d2e08f632e21150b4621111eba10123f
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2025-09-29 20:28:26 -0400

    Initialize and swap loop_node_range in ExtrusionEntityCollection
    Added initialization of loop_node_range in the constructor and included it in the swap method to ensure proper state management of ExtrusionEntityCollection instances.

--- src/libslic3r/ExtrusionEntityCollection.cpp
+++ src/libslic3r/ExtrusionEntityCollection.cpp
@@ -21,7 +21,7 @@ void filter_by_extrusion_role_in_place(ExtrusionEntitiesPtr &extrusion_entities,
 }
 
 ExtrusionEntityCollection::ExtrusionEntityCollection(const ExtrusionPaths &paths)
-    : no_sort(false)
+    : no_sort(false), loop_node_range(0, 0)
 {
     this->append(paths);
 }
@@ -41,6 +41,7 @@ void ExtrusionEntityCollection::swap(ExtrusionEntityCollection &c)
 {
     std::swap(this->entities, c.entities);
     std::swap(this->no_sort, c.no_sort);
+    std::swap(this->loop_node_range, c.loop_node_range);
 }
 
 void ExtrusionEntityCollection::clear()
