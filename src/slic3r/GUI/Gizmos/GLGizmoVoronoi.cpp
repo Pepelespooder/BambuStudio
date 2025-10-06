751d207dd22481f1adcb0ac3237b8fe612a8fdc7
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2025-10-05 20:57:53 -0400

    Update GLGizmoVoronoi.cpp

--- src/slic3r/GUI/Gizmos/GLGizmoVoronoi.cpp
+++ src/slic3r/GUI/Gizmos/GLGizmoVoronoi.cpp
@@ -1,6 +1,7 @@
 #include "GLGizmoVoronoi.hpp"
 #include "slic3r/GUI/GLCanvas3D.hpp"
 #include "slic3r/GUI/GUI_App.hpp"
+#include "slic3r/GUI/GUI.hpp"
 #include "slic3r/GUI/Plater.hpp"
 #include "slic3r/GUI/format.hpp"
 #include "slic3r/GUI/Camera.hpp"
